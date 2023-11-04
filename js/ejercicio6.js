a60a38ff97a4aaf56795651415b9f27aa59ea4a3
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-04 11:40:40 -0300

    ejercicio 6 del practico 2 resuelto

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,6 +1,5 @@
 /*
 6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
-
 1
 22
 333
@@ -9,3 +8,9 @@
 666666
 .......
 */
+for( let filas = 1; filas <= 30; filas++){
+    for( let i = 0; i < filas; i++){
+        document.write(filas)
+    }
+    document.write('<br>')
+}

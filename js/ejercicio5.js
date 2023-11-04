d1b9c62556314172610a240c63ed6ccca58ff0fa
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-04 10:51:45 -0300

    ejercicio 5 del practico 2 resuelto

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -11,93 +11,16 @@ Input:  40773821
 Output: ‘L’
 */
 
-    let dni = parseInt(prompt("Ingrese su DNI"));
-    const letras = "TRWAGMYFPDXBNJZSQVHLCKE"
+const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+while (confirm("Ingrese un DNI")) {
+    const dni = parseInt(prompt("Ingrese un DNI"));
     if (!isNaN(dni) && dni > 0 && dni <= 99999999) {
         let resto = (dni % 23);
         console.log(resto);
         alert(letras.charAt(resto));
-    } else {
+    }
+    else {
         alert("Ingreso inválido");
     }
-
-
-
-// let dni = parseInt(prompt("Ingrese su DNI"));
-// if (!isNaN(dni) && dni > 0 && dni <= 99999999) {
-//     let resto = dni % 23;
-//     console.log(resto);
-//     if (resto === 0) {
-//         alert("T")
-//     } else if (resto === 1) {
-//         alert("R")
-//     }
-//     else if (resto === 2) {
-//         alert("W")
-//     }
-//     else if (resto === 3) {
-//         alert("A")
-//     }
-//     else if (resto === 4) {
-//         alert("G")
-//     }
-//     else if (resto === 5) {
-//         alert("M")
-//     }
-//     else if (resto === 6) {
-//         alert("Y")
-//     }
-//     else if (resto === 7) {
-//         alert("F")
-//     }
-//     else if (resto === 8) {
-//         alert("P")
-//     }
-//     else if (resto === 9) {
-//         alert("D")
-//     }
-//     else if (resto === 10) {
-//         alert("X")
-//     }
-//     else if (resto === 11) {
-//         alert("B")
-//     }
-//     else if (resto === 12) {
-//         alert("N")
-//     }
-//     else if (resto === 13) {
-//         alert("J")
-//     }
-//     else if (resto === 14) {
-//         alert("Z")
-//     }
-//     else if (resto === 15) {
-//         alert("S")
-//     }
-//     else if (resto === 16) {
-//         alert("Q")
-//     }
-//     else if (resto === 17) {
-//         alert("V")
-//     }
-//     else if (resto === 18) {
-//         alert("H")
-//     }
-//     else if (resto === 19) {
-//         alert("L")
-//     }
-//     else if (resto === 20) {
-//         alert("C")
-//     }
-//     else if (resto === 21) {
-//         alert("K")
-//     }
-//     else if (resto === 22) {
-//         alert("E")
-//     }
-// } else {
-//     alert("Ingreso inválido")
-// }
-
-
-
+}

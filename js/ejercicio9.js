3e364b6dbbf79a122c1eec6878f47c1193c5d020
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-04 12:48:10 -0300

    ejercicio 9 del practico 2 resuelto

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -13,3 +13,16 @@
 9 (Múltiplo de 9)
 10
 */
+for (let numero = 1; numero <= 500; numero++) {
+    document.write(numero);
+    if (numero % 4 === 0) {
+        document.write(' (múltiplo de 4)')
+    }
+    if (numero % 9 === 0) {
+        document.write(' (múltiplo de 9)')
+    }
+    document.write('<br>')
+    if (numero % 5 === 0) {
+        document.write('-----------<br>')
+    }
+}

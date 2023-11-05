5b45f4c6e597927b53d02c84ed598a8ddd0dbcbc
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-05 00:33:29 -0300

    cambie el let por const en la variable texto

--- js/ejercicio14.js
+++ js/ejercicio14.js
@@ -1,7 +1,7 @@
 /*14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 */
 
-let texto = prompt("Ingrese un texto")
+const texto = prompt("Ingrese un texto")
 
 for (caracter = 0; caracter < texto.length; caracter++){
     if(caracter < texto.length - 1){
